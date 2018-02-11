2541d3009d21eb9dce3cc269a0d3124b4d750923
Sonal Ranjit <sonal.ranjit3@gmail.com>
2018-02-11 14:46:25 -0500

    started css

--- tnff/views.py
+++ tnff/views.py
@@ -5,3 +5,7 @@ from . import application
 @application.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@application.route('/about',methods=['GET'])
+def about():
+    return render_template('about.html')
