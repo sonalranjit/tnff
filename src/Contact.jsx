04d158e736c7c634f1d857c8a40014d694e8bcc0
Sonal Ranjit <sonal.ranjit3@gmail.com>
2018-02-18 14:08:15 -0500

    started on react components

---
+++ src/Contact.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>GOT QUESTIONS?</h2>
+                <p> The easiest thing to do is post on</p>
+            </div>
+        );
+    }
+}
+export default Contact;
